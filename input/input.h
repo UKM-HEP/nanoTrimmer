bd172e9ece18fa9638ac67bf6e1e6459620dcc0d
SiewYan Hoh <siew.yan.hoh@gmail.com>
2023-01-28 18:38:07 +0800

    snapshot

--- input/input.h
+++ input/input.h
@@ -12,24 +12,24 @@ auto runningInput( T &df , Helper::config_t &cfg ){
   cfg.Luminosity = 4430; // in pb^{-1}
 
   // What is the HLT trigger you used for your analysis ?
-  cfg.HLT = "HLT_IsoMu30_eta2p1";
+  cfg.HLT = "HLT_Dimuon0_Jpsi_Muon";
 
   // What is the type of lepton you are working with ?
   cfg.Flavor = "Muon";
   std::string Id = (cfg.Flavor == "Electron") ? "11" : "13";
 
   // What is the HLT object you are working with ?
-  cfg.HLTobject = "TrigObj_isou";
+  cfg.HLTobject = "TrigObj_jpsiu";
   if (cfg.isMC) cfg.HLTobject = "GenPart";
   
   // What is the MINIMUM transverse momentum for your Tag ?
-  cfg.kMinTagPt = 35; // GeV
+  cfg.kMinTagPt = 5; // GeV
 
   // What is the MAXIMUM pseudorapidity for your Tag ?
   cfg.kMaxTagEta = 2.4;
 
   // What is the MINIMUM transverse momentum for your Probe ?
-  cfg.kMinProbePt = 25; // GeV
+  cfg.kMinProbePt = 3; // GeV
 
   // What is the MAXIMUM pseudorapidity for your Probe ?
   cfg.kMaxProbeEta = 2.4;
@@ -40,10 +40,10 @@ auto runningInput( T &df , Helper::config_t &cfg ){
   cfg.xsec =  1177.3; // pb
 
   // What is the MAXIMUM mass of your favorite resonance?
-  cfg.kMaxMass = 120.; // GeV
+  cfg.kMaxMass = 2.; // GeV
 
   // What is the MINIMUM mass of your favorite resonance?
-  cfg.kMinMass = 60.; // GeV
+  cfg.kMinMass = 5.; // GeV
 
   // What is the defined WP for Tag?
   // Electron : 0 fail ; 1 veto ; 2 loose ; 3 medium ; 4 tight
@@ -73,7 +73,7 @@ auto runningInput( T &df , Helper::config_t &cfg ){
   df = df
     .Filter( cfg.HLT+"==1" , "PRE-SELECTION: Passing "+ cfg.HLT +" trigger selection" )
     .Filter( "!(abs(Tag_eta)>= 1.4442 && abs(Tag_eta)<=1.566)" , "PRE-SELECTION: Selecting event containing Tag candidates well covered inside the detector" )
-    .Filter( "abs(Tag_pdgId) == "+Id+" && Tag_pdgId+Probe_pdgId == 0" , "PRE-SELECTION: Selecting event containing Tag and Probe pair made up of 2 "+cfg.Flavor     )
+    .Filter( "abs(Tag_pdgId) == "+Id+" && Tag_pdgId+Probe_pdgId == 0" , "PRE-SELECTION: Selecting event containing Tag and Probe pair made up of 2 "+cfg.Flavor+" (pdgId = "+Id+")"     )
     .Filter( "Tag_wp == 4 ", "PRE-SELECTION: Selecting event containing Tag with working point tight" )
     ;
   
--- scripts/runTrim.sh
+++ scripts/runTrim.sh
@@ -4,5 +4,5 @@ set -e
 
 # process the samples in the folder
 # -b batch;
-python ./scripts/trim.py -d ${PWD}/samples_test/Run2012B_SingleElectron -o $PWD -n 3 -c 2
-python ./scripts/trim.py -d ${PWD}/samples_test/DYJetsToLL_M-50_TuneZ2Star -o $PWD -n 3 -c 2
+python ./scripts/trim.py -d ${PWD}/samples_test/Run2011B_MuOnia -o $PWD -n 3 -c 3
+#python ./scripts/trim.py -d ${PWD}/samples_test/JPsiToMuMu_2MuPEtaFilter -o $PWD -n 3 -c 2
