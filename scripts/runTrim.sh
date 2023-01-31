3bfa4b12226d159daebf45efc8c3e2511056d1f4
SiewYan Hoh <siew.yan.hoh@gmail.com>
2023-01-31 11:15:56 +0800

    added time stamp compute

--- scripts/runTrim.sh
+++ scripts/runTrim.sh
@@ -2,7 +2,12 @@
 
 set -e
 
+start_time=$SECONDS
+
 # process the samples in the folder
-# -b batch;
 python trim.py -d ${PWD}/samples_test/Run2012B_SingleElectron -o $PWD -n 3 -c 2
 python trim.py -d ${PWD}/samples_test/DYJetsToLL_M-50_TuneZ2Star -o $PWD -n 3 -c 2
+
+elapsed=$(( SECONDS - start_time ))
+
+eval "echo Elapsed time: $(date -ud "@$elapsed" +'$((%s/3600/24)) days %H hr %M min %S sec')"
