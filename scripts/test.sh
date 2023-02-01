7bfc680b7fc2560b648d01222a26ad263fd6093c
SiewYan Hoh <siew.yan.hoh@gmail.com>
2023-02-01 19:13:00 +0800

    added test file

---
+++ scripts/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+start_time=$SECONDS
+
+# process the samples in the folder
+python ./scripts/trim.py -d ${PWD}/samples_test/Run2012B_SingleMu -o $PWD -n 3 -c 4
+python ./scripts/trim.py -d ${PWD}/samples_test/DYJetsToLL_M-50_TuneZ2Star -o $PWD -n 3 -c 4
+
+elapsed=$(( SECONDS - start_time ))
+echo
+eval "echo Elapsed time: $(date -ud "@$elapsed" +'$((%s/3600/24)) days %H hr %M min %S sec')"
