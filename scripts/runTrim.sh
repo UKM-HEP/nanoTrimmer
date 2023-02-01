e47950819b211a9db00a8babfddad44c4e262360
SiewYan <siew.yan.hoh@gmail.com>
2023-02-01 15:32:13 +0800

    add 2.4 factor

--- scripts/runTrim.sh
+++ scripts/runTrim.sh
@@ -5,8 +5,8 @@ set -e
 start_time=$SECONDS
 
 # process the samples in the folder
-python ./scripts/trim.py -d /disk01/cms-open-data/prod_v3/8TeV_tnp/RunI/8TeV/Run2012A_SingleMu -o $PWD -n 30 -c 5
-python ./scripts/trim.py -d /disk01/cms-open-data/prod_v3/8TeV_tnp/RunI/8TeV/DYJetsToLL_M-50_TuneZ2Star -o $PWD -n 30 -c 5
+python ./scripts/trim.py -d /disk01/cms-open-data/prod_v3/8TeV_tnp/RunI/8TeV/Run2012A_SingleMu -o $PWD -n 30 -c 7
+python ./scripts/trim.py -d /disk01/cms-open-data/prod_v2/8TeV_tnp/RunI/8TeV/DYJetsToLL_M-50_TuneZ2Star -o $PWD -n 30 -c 7
 
 elapsed=$(( SECONDS - start_time ))
 echo
